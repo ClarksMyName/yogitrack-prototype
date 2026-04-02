d57b8e14eaf6de1cf2b390f438bf6cf28372c6b3
ClarksMyName <ryanclark16123@gmail.com>
2026-04-02 00:35:58 -0400

    Add updated project files

--- yogiserver.cjs
+++ yogiserver.cjs
@@ -9,6 +9,10 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/api/instructor", require("./routes/instructorRoutes.cjs"));
+app.use("/api/class", require("./routes/classRoutes.cjs"));
+app.use("/api/customer", require("./routes/customerRoutes.cjs"));
+app.use("/api/package", require("./routes/packageRoutes.cjs"));
+app.use("/api/checkin", require("./routes/checkinRoutes.cjs"));
 
 
 
